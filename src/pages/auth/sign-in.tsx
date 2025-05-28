fd8cac3462651dd42d79e2c45b5a702231ea91d0
Cristhoffer Nunes <cristhoffer.nunes.santos@gmail.com>
2025-05-27 23:38:20 -0300

    feat: create sign-up restaurant page

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,5 +1,6 @@
 import { Helmet } from "react-helmet-async";
 import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 import { toast } from "sonner";
 import { z } from "zod";
 
@@ -29,7 +30,7 @@ export function SignIn() {
           onClick: () => handleSignIn(data),
         },
       });
-    } catch (error) {
+    } catch {
       toast.error("Credenciais inválidas");
     }
   }
@@ -38,6 +39,9 @@ export function SignIn() {
     <>
       <Helmet title="Sign In" />
       <div className="p-8">
+        <Button variant="ghost" asChild className="absolute top-8 right-8">
+          <Link to="/sign-up">Novo estabelecimento</Link>
+        </Button>
         <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
