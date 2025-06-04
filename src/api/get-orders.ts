0c89795b1366081fa08d9b7438ebab4a4ca67605
Cristhoffer Nunes <cristhoffer.nunes.santos@gmail.com>
2025-06-03 21:46:25 -0300

    feat: add order list connected to backend

--- src/api/get-orders.ts
+++ src/api/get-orders.ts
@@ -2,6 +2,9 @@ import { api } from "@/lib/axios";
 
 export interface GetOrdersQuer {
   pageIndex?: number | null;
+  orderId?: string | null;
+  customerName?: string | null;
+  status?: string | null;
 }
 
 export interface GetOrdersResponse {
@@ -19,10 +22,18 @@ export interface GetOrdersResponse {
   };
 }
 
-export async function getOrders({ pageIndex }: GetOrdersQuer) {
+export async function getOrders({
+  pageIndex,
+  orderId,
+  customerName,
+  status,
+}: GetOrdersQuer) {
   const response = await api.get<GetOrdersResponse>("/orders", {
     params: {
       pageIndex,
+      orderId,
+      customerName,
+      status,
     },
   });
 
